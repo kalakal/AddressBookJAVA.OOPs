7800d567d073f20e5be1a58e087645d5e6d33f42
kalakal <sayedfaizanqadri@gmail.com>
2023-03-04 15:10:00 +0530

    Added Usecase For EditContact

--- src/com/bridgelab/addressbook/AddAddressBook.java
+++ src/com/bridgelab/addressbook/AddAddressBook.java
@@ -5,8 +5,9 @@ import java.util.Scanner;
 
 public class AddAddressBook {
     Scanner scanner = new Scanner(System.in);
-    Contacts contact=new Contacts();
+    Contacts contact = new Contacts();
     ArrayList<Contacts> bookList = new ArrayList<Contacts>();
+
     public void addContact() {
         System.out.println("Enter the First name");
         String firstName = scanner.nextLine();
@@ -43,7 +44,8 @@ public class AddAddressBook {
         bookList.add(contact);
     }
 
-    public void display() {System.out.println("Enter first name to diplay the contact details");
+    public void display() {
+        System.out.println("Enter first name to diplay the contact details");
         String personName = scanner.next();
 
         if (bookList.size() > 0) {
@@ -63,5 +65,55 @@ public class AddAddressBook {
         }
     }
 
+    public void editContacts() {
+        System.out.println("Enter the First Name of a person to edit:");
+        String name = scanner.nextLine();
+
+        if (name.equalsIgnoreCase(contact.getFirstName())) {
+            System.out.println("Enter which contact you want to edit");
+            System.out.println("1. First name \n2. Last Name \n3.Address \n4. city \n5. State \n6. Zip \n7. Mobile Number \n8. Email");
+            int option = scanner.nextInt();
+            switch (option) {
+                case 1:
+                    System.out.println("Enter new first name");
+                    contact.setFirstName(scanner.next());
+                    break;
+                case 2:
+                    System.out.println("Enter new last name");
+                    contact.setLastName(scanner.next());
+                    break;
+                case 3:
+                    System.out.println("Enter new addres ");
+                    contact.setAddress(scanner.next());
+                    break;
+                case 4:
+                    System.out.println("Enter new city name");
+                    contact.setCity(scanner.next());
+                    break;
+                case 5:
+                    System.out.println("Enter new state name");
+                    contact.setState(scanner.next());
+                    break;
+                case 6:
+                    System.out.println("Enter new zip number");
+                    contact.setZip(scanner.next());
+                    break;
+                case 7:
+                    System.out.println("Enter new mobil number");
+                    contact.setMobileNumber(scanner.next());
+                    break;
+                case 8:
+                    System.out.println("Enter new email ");
+                    contact.setFirstName(scanner.next());
+                    break;
+
+            }
+        } else {
+            System.out.println("Invalid First Name \n Please enter Valid First name: ");
+            editContacts();
+        }
+
+
+    }
 }
 
